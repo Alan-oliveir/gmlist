67f83a09318b3926296c97ecb6f7f11b3a84f5eb
Alan-oliveir <alandeog@poli.ufrj.br>
2023-05-14 00:29:31 -0300

    Upload files

---
+++ src/test/java/com/devsuperior/gmlist/GmlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.gmlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
